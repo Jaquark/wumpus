cbed21d143a9bb33dc292cb71626b3686047cdcf
Christopher Nerf <nerf.christopher@gmail.cm>
2019-08-09 19:39:34 -0400

    Project 3 Checking adjacencies create correct entry into KB.

--- wumpus.py
+++ wumpus.py
@@ -15,10 +15,10 @@ boardheight = 0
 
 
 
-KB = { }
 '''
-    Knowledge base will be of the form 'pos_x,pos_y' : [stench, breeze, glitter]
+    Ever object square that is adjacent to another square
 '''
+adjacencies = {}
 
 class board_object():
     def __init__(self, x, y, name, facing=''):
@@ -37,6 +37,42 @@ def checkForEndCondition(player,thingsOnTheBoard):
                 return 'were eaten'
         else:
             return ''
+
+def add_precept(KB, playerObject, boardObjects):
+
+    squareValue = ['','','']
+
+    print(adjacencies[playerObject.position])
+    
+    for adjSquare in adjacencies[playerObject.position]:
+        for obj in boardObjects:
+            if obj.position == adjSquare:
+                print(obj.position)
+                if obj.obj_type == 'Wumpus':
+                    squareValue[0] = 'Stench'
+                if obj.obj_type == 'Pit':
+                    squareValue[1] = 'Breeze'
+            if obj.obj_type == 'Gold' and obj.position == playerObject.position :
+                squareValue[2] = 'Glitter'
+    KB.update( { playerObject.position : squareValue })        
+    return KB
+
+def calculateAdjacenies(x,y,n):
+    adjacentSquares = []
+
+    #check north
+    if y + 1 <= n:
+        adjacentSquares.append('{0},{1}'.format(x,y+1))
+    #check south
+    if y - 1 >= 1:
+        adjacentSquares.append('{0},{1}'.format(x,y-1))
+    #check east
+    if x + 1 <= n:
+        adjacentSquares.append('{0},{1}'.format(x+1,y))
+    #check west
+    if x - 1 >= 1:
+        adjacentSquares.append('{0},{1}'.format(x-1,y))
+    return adjacentSquares
 
 def main():
     #import game state
@@ -58,30 +94,45 @@ def main():
     gameState.reverse()
     #print(gameState)
 
+    '''
+    Knowledge base will be of the form 'pos_x,pos_y' : ['Stench','Pit','Gold'] 
+    
+    '''
+    KB = {}
+
     #first we need to populate the entire knowledge base with Socrates definition of true knowledge
     for x in range(0,len(gameState)) :
         for y in range(0,len(gameState[x])):
+                coordinatesAsString = '{0},{1}'.format(y+1, x+1)
                 if( gameState[x][y] == 'W'):
-                    boardObjects.append( board_object(x+1,y+1,'Wumpus') )
+                    boardObjects.append( board_object(y+1, x+1,'Wumpus') )
+                    print('wumpus at {0},{1}'.format(y+1, x+1))
                 elif(gameState[x][y] == 'P'):
-                    boardObjects.append( board_object(x+1,y+1,'Pit') )
+                    boardObjects.append( board_object(y+1, x+1,'Pit') )
+                    print('pit at {0},{1}'.format(y+1, x+1))
                 elif(gameState[x][y] == 'G'):
-                    boardObjects.append( board_object(x+1,y+1,'Gold') )
-                KB.update( {'{0},{1}'.format(x+1,y+1) : [None, None, None] })
+                    boardObjects.append( board_object(y+1, x+1,'Gold') )
+                    print('gold at {0},{1}'.format(y+1, x+1))
+                adjacencies.update ( { coordinatesAsString : calculateAdjacenies(y+1, x+1, len(gameState)) } )
     
     #Then we want to add the person to the game, at position 1,1
-    playerObject = board_object(1,1,'Player','East')
+    #playerObject = board_object(1,1,'Player','East')
+
+    #playerObject = board_object(1,2,'Player','East') #- KB 1,2 == 2
+    #playerObject = board_object(2,1,'Player','East')#- KB 2,1 == 1
+    playerObject = board_object(2,3,'Player','East')#- KB 2,3 == 7 
 
     endGameVerbage = ''
 
-    while(endGameVerbage == ''):
-        add_precept(KB, playerObject, boardObjects)
-
-
-        endGameVerbage = checkForEndCondition(playerObject,boardObjects)
+    #while(endGameVerbage == ''):
+        
+    #    endGameVerbage = checkForEndCondition(playerObject,boardObjects)
+    #    KB = add_precept(KB, playerObject, boardObjects)
     
+    #KB = add_precept(KB, playerObject, boardObjects)
 
-    
+    #print(KB)
+
 
     
 
